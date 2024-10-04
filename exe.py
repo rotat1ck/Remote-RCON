d5dcd1b3215471eaf7f602b874baa4a8040cf8fe
Zepo4ka <dgavrilov2007@gmail.com>
2024-10-04 18:58:43 +0300

    get list

--- exe.py
+++ exe.py
@@ -1,1 +1,15 @@
-#exe 1111
+from rcon.source import Client
+
+def get_list_of_players():
+    with Client('77.37.246.6', 2589, passwd='zalupa1488') as client:
+        response = client.run('list')
+        
+    prefix, player_names = response.split(':')
+    player_names = player_names.strip()
+    player_names = player_names.split(', ')
+    words = prefix.split()
+    numbers = [word for word in words if word.isdigit()]
+    print(f"     {numbers[0]}/{numbers[1]}")
+    for i in player_names:
+        print(i)
+get_list_of_players()
