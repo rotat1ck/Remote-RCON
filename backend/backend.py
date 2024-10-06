773927923cf5144b4110a6daa3e9716a6233437c
rotatick <you@example.com>
2024-10-06 13:31:49 +0300

    version 1.0

--- backend/backend.py
+++ backend/backend.py
@@ -1,4 +1,7 @@
-import requests, socket, sys, json
+import requests
+import socket
+import json
+import threading
 
 def checkhash(hash_value, conn, addr):
     data = {"hash": hash_value}
@@ -15,44 +18,68 @@ def checkhash(hash_value, conn, addr):
         except json.JSONDecodeError:
             print("Invalid JSON response:", response.text)
     else:
+        conn.sendall(f"Access denied".encode())
         print("Error:", response.status_code)
-    conn.close()
 
-def handleCommand(command, accesscode):
+
+def handleCommand(conn, command, accesscode):
     data = {"code": accesscode}
     response = requests.post("http://localhost:8000/main", json=data)
     if response.status_code == 200:
         try:
             data = response.json()
             userid = data.get("id")
-            oplvl = data.get("oplevel")
+            oplvl = data.get("oplvl")
             code = data.get("accesscode")
+            # выполнение rcon команды
+            import rconexec
+            dataToSend = rconexec.check(command, oplvl)
+            print(f"Пользователь {userid} | Выполнил команду {command}")
+            conn.sendall(f"{dataToSend}".encode())
         except json.JSONDecodeError:
             print("Invalid JSON response:", response.text)
     else:
         print("Error:", response.status_code)
-def server():
-    host, port = "0.0.0.0", 7777
-    server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-    server.bind((host, port))
-    server.listen(10)
-    
-    while True:
-        try:
-            conn, addr = server.accept()
-            data = conn.recv(1024) 
+
+
+def client_handler(conn, addr):
+    print(f"Connected by {addr}")
+    try:
+        while True:
+            data = conn.recv(1024)
+            if not data:
+                break
             hash_value = data.decode()
-            if hash_value[0] != '/':
+            if hash_value == 'keepalive':
+                continue
+            elif hash_value[0] != '/':
                 checkhash(hash_value, conn, addr)
             else:
-                command = hash_value
-                data = conn.recv(1024) 
+                command = hash_value[1:]
+                data = conn.recv(1024)
                 accesscode = data.decode()
-                handleCommand(command, accesscode)
-        except KeyboardInterrupt:
-            server.close()
-            exit()
+                handleCommand(conn, command, accesscode)
+    finally:
+        print(f"Connection with {addr} closed.")
+
+
+def server():
+    host, port = "0.0.0.0", 7777
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as server:
+        server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        server.bind((host, port))
+        server.listen(10)
+        print("Server is listening on port", port)
+
+        while True:
+            try:
+                conn, addr = server.accept()
+                client_thread = threading.Thread(target=client_handler, args=(conn, addr))
+                client_thread.daemon = True
+                client_thread.start()
+            except KeyboardInterrupt:
+                print("Server shutting down.")
+                break
 
 if __name__ == "__main__":
     server()
--- exe.py
+++ exe.py
@@ -1,133 +1,215 @@
+import threading
+import time
+import socket
 import dearpygui.dearpygui as dpg
-from rcon.source import Client
-import functools
-commands_history = []
+import dearpygui.dearpygui as dpg
+import hashlib
+import socket
 
-def append_to_command_input(sender, app_data, user_data):
-    player_name = user_data
-    current_text = dpg.get_value("command_input")
-    new_text = f"{current_text} {player_name}".strip()  # Append the player's name to the existing text
-    dpg.set_value("command_input", new_text)
+def auth():
+    commands_history = []
+    HOST, PORT = "77.37.246.6", 7777
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    sock.connect((HOST, PORT))  # Connect once
 
-# Updated get_list_of_players function
-def get_list_of_players(sender):
-    with Client('77.37.246.6', 2589, passwd='zalupa1488') as client:
-        response = client.run('list')
+    def keep_alive():
+        while True:
+            try:
+                sock.sendall(bytes('keepalive', encoding="utf-8"))
+                time.sleep(60)  # Wait for 60 seconds before sending the next keep-alive
+            except Exception as e:
+                print(f"Keep-alive error: {e}")
+                break
+
+    # Start the keep-alive thread
+    threading.Thread(target=keep_alive, daemon=True).start()
+
+    def get_data(command, accesscode):
+        response = ""
+        try:
+            sock.sendall(bytes(command, encoding="utf-8"))
+            sock.sendall(bytes(accesscode, encoding="utf-8"))
+
+            response = sock.recv(1024)
+            response = response.decode("utf-8")
+        except Exception as e:
+            print(e)
+        return response
+
+    def append_to_command_input(sender, app_data, user_data):
+        player_name = user_data
+        current_text = dpg.get_value("command_input")
+        new_text = f"{current_text} {player_name}".strip()
+        dpg.set_value("command_input", new_text)
+
+    def get_list_of_players(sender):
+        response = get_data(command='/list', accesscode=accesscode)
+        if ':' in response:
+            prefix, player_names = response.split(':')
+            player_names = player_names.strip()
+            player_names = player_names.split(', ')
+            words = prefix.split()
+            numbers = [word for word in words if word.isdigit()]
+            
+            if dpg.does_item_exist("player_list"):
+                dpg.delete_item("player_list")
+            
+            with dpg.child_window(width=185, height=233, parent="right_column", tag="player_list", border=False):
+                dpg.add_text(f"{numbers[0]}/{numbers[1]} Players: ")
+                for player in player_names:
+                    dpg.add_button(label=player, callback=append_to_command_input, user_data=player)
+            update_menu_items()
+
+    def execute_command(sender, data):
+        command = dpg.get_value("command_input")
+        commands_history.append(command)
+        response = get_data(command=command, accesscode=accesscode)
         
-        prefix, player_names = response.split(':')
-        player_names = player_names.strip()
-        player_names = player_names.split(', ')
-        words = prefix.split()
-        numbers = [word for word in words if word.isdigit()]
-        
-        if dpg.does_item_exist("player_list"):
-            dpg.delete_item("player_list")
-        
-        with dpg.child_window(width=185, height=233, parent="right_column", tag="player_list", border=False):
-            dpg.add_text(f"{numbers[0]}/{numbers[1]} Players: ")
-            for player in player_names:
-                # Create a button for each player and append their name to the command input when clicked
-                dpg.add_button(label=player, callback=append_to_command_input, user_data=player)
-    update_menu_items()
+        commands_history.append(response)
 
-
-
-def execute_command(sender, data):
-    command = dpg.get_value("command_input")
-    commands_history.append(command)
-    with Client('77.37.246.6', 2589, passwd='zalupa1488') as client:
-        response = client.run(command)
-        if response != "":
-            commands_history.append(response)
-        else:
-            commands_history.append("No answer")
         if dpg.does_item_exist("output_box"):
             dpg.delete_item("output_box")
         with dpg.child_window(width=385, height=234, parent="left_column", tag="output_box",border=False):
             for text in commands_history:
                 dpg.add_text(text)
 
-def get_player_names():
-    with Client('77.37.246.6', 2589, passwd='zalupa1488') as client:
-        response = client.run('list')
+    def get_player_names():
+        response = get_data(command='/list', accesscode=accesscode)
+        if ':' in response:
+            prefix, player_names = response.split(':')
+            player_names = player_names.strip()
+            player_names = player_names.split(', ')
+            return player_names
+        else:
+            print("Error: Invalid response from server")
+            return []
 
-        prefix, player_names = response.split(':')
-        player_names = player_names.strip()
-        player_names = player_names.split(', ')
-    return player_names
+    def update_menu_items():
+        if dpg.does_item_exist("DoxMenu"):
+            dpg.delete_item("DoxMenu", children_only=True)
+        if dpg.does_item_exist("LightMenu"):
+            dpg.delete_item("LightMenu", children_only=True)
+        create_menu_items()
 
-def update_menu_items():
-    if dpg.does_item_exist("DoxMenu"):
-        dpg.delete_item("DoxMenu", children_only=True)
-    if dpg.does_item_exist("LightMenu"):
-        dpg.delete_item("LightMenu", children_only=True)
-    create_menu_items()
-    
-def dox_player_callback(player_name):
-    def callback(sender, data):
-        dox_player(player_name, sender)
-    return callback
+    def dox_player_callback(player_name):
+        def callback(sender, data):
+            dox_player(player_name, sender)
+        return callback
 
-def light_player_callback(player_name):
-    def callback(sender, data):
-        light_player(player_name, sender)
-    return callback
+    def light_player_callback(player_name):
+        def callback(sender, data):
+            light_player(player_name, sender)
+        return callback
 
-def create_menu_items():
-    player_names = get_player_names()
-    for player in player_names:
-        dpg.add_menu_item(label=player, parent="DoxMenu", callback=dox_player_callback(player))
-    for player in player_names:
-        dpg.add_menu_item(label=player, parent="LightMenu", callback=light_player_callback(player))
+    def create_menu_items():
+        player_names = get_player_names()
+        for player in player_names:
+            dpg.add_menu_item(label=player, parent="DoxMenu", callback=dox_player_callback(player))
+        for player in player_names:
+            dpg.add_menu_item(label=player, parent="LightMenu", callback=light_player_callback(player))
 
-def light_player(player_name, sender):
-    with Client('77.37.246.6', 2589, passwd='zalupa1488') as client:
-        response = client.run('list')
-        client.run(f'execute at {player_name} run summon minecraft:lightning_bolt ~ ~ ~')
+    def light_player(player_name, sender):
+        response = get_data(command=f'/light {player_name}', accesscode=accesscode)
 
-def dox_player(player_name, sender):
-    with Client('77.37.246.6', 2589, passwd='zalupa1488') as client:
-        client.run(f'effect give {player_name} minecraft:blindness 3 1 true')
-        client.run(f'playsound minecraft:entity.ghast.scream ambient {player_name} ~ ~ ~ 10 0.5 1')
-        client.run(f'title {player_name} times 0 40 0')
-        client.run(f'title {player_name} subtitle {{\"text\":\"\u0418 \u0441\u043f\u043e\u0440\u0442\u0438\u043a\u043e\u0432\"}}')
-        client.run(f'title {player_name} title {{\"text\":\"\u0416\u0434\u0438 \u0434\u043e\u043a\u0441. \u0441\u0432\u0430\u0442\"}}')
-        client.run(f'playsound minecraft:entity.ghast.scream ambient {player_name} ~ ~ ~ 10 0.5 1')
-        client.run(f'playsound minecraft:entity.ghast.scream ambient {player_name} ~ ~ ~ 10 0.5 1')
-        client.run(f'playsound minecraft:entity.ghast.scream ambient {player_name} ~ ~ ~ 10 0.5 1')
+    def dox_player(player_name, sender):
+        response = get_data(command=f'/dox {player_name}', accesscode=accesscode)
 
-dpg.create_context()
+    def cleanup():
+        sock.close()
+        print("Socket connection closed.")
 
-with dpg.window(tag="RemoteRCON"):
-    with dpg.menu_bar():
-        with dpg.menu(label="Troll"):
-            with dpg.menu(label="Lightning", tag="LightMenu"):
-                dpg.add_menu_item(label="LoadPlayers")
+    login = ''
+    password = ''
 
-            with dpg.menu(label="Dox", tag="DoxMenu"):
-                dpg.add_menu_item(label="LoadPlayers")
+    dpg.create_context()
 
-        dpg.add_menu_item(label="Load Players", callback=get_list_of_players)
+    def handlelogin(sender):
+        global login
+        login = dpg.get_value(sender)
 
-    with dpg.group(horizontal=True):
-        with dpg.child_window(width=400, height=250, tag="left_column"):
-            with dpg.child_window(width=385, height=234, tag="output_box",border=False):
+    def handlepass(sender):
+        global password
+        password = dpg.get_value(sender)
+
+    def createHash(sender, data):
+        global login
+        global password
+        data = (login + password).encode('utf-8')
+        hash_value = hashlib.sha256(data).hexdigest()
+        sendHash(hash_value)
+
+    def sendHash(hash_value):
+        data = hash_value
+
+        try:
+            sock.sendall(bytes(data, encoding="utf-8"))
+            global accesscode
+            received = sock.recv(1024)
+            received = received.decode("utf-8")
+            if received != 'Access denied':
+                accesscode = received
+                login_callback()
+            else:
+                dpg.set_value("auth_feedback", "Access denied!")
+        except Exception as e:
+            print("Connection error:", e)
+
+
+    with dpg.window(tag="Auth", label="Authentication"):
+        dpg.set_global_font_scale(2)
+        dpg.add_text("Remote RCON", indent=205)
+
+        dpg.add_input_text(hint="Enter login", callback=handlelogin, indent=160, width=250)
+        dpg.add_input_text(hint="Enter password", callback=handlepass, indent=160, width=250, password=True)
+        dpg.add_button(label="Log in", callback=createHash, indent=235, height=35, tag="login_button")
+        dpg.add_text("", tag="auth_feedback", indent=195)
+        
+    with dpg.window(tag="RemoteRCONWindow", label='Remote RCON', show=False):
+        with dpg.menu_bar():
+            with dpg.menu(label="Troll"):
+                with dpg.menu(label="Lightning", tag="LightMenu"):
+                    dpg.add_menu_item(label="LoadPlayers")
+
+                with dpg.menu(label="Dox", tag="DoxMenu"):
+                    dpg.add_menu_item(label="LoadPlayers")
+
+            dpg.add_menu_item(label="Load Players", callback=get_list_of_players)
+
+        with dpg.group(horizontal=True):
+            with dpg.child_window(width=400, height=250, tag="left_column"):
+                with dpg.child_window(width=385, height=234, tag="output_box",border=False):
+                    pass
+
+            with dpg.child_window(width=200, height=250, tag="right_column"):
                 pass
 
-        with dpg.child_window(width=200, height=250, tag="right_column"):
-            pass
+        with dpg.group(horizontal=True):
+            with dpg.child_window(width=400, height=58):
+                dpg.add_input_text(tag="command_input", hint="/help")
+                dpg.add_button(label="Run", callback=execute_command)
+            dpg.add_text("v. 1.0")
+                
+    dpg.create_viewport(title='Authentication', width=600, height=300)
+    dpg.setup_dearpygui()
+    dpg.show_viewport()
+    dpg.set_viewport_resizable(False)
+    dpg.set_primary_window("Auth", True)
+    dpg.start_dearpygui()
 
-    with dpg.group(horizontal=True):
-        with dpg.child_window(width=400, height=58):
-            dpg.add_input_text(tag="command_input", hint="/help")
-            dpg.add_button(label="Run", callback=execute_command)
+def login_callback():
+    # Hide the authentication window
+    dpg.hide_item("Auth")
+    # Show the RemoteRCON window
+    dpg.show_item("RemoteRCONWindow")
+    # Change the viewport title to reflect the new window
+    dpg.set_viewport_title("RemoteRCON")
+    # Resize the viewport if needed
+    dpg.set_viewport_width(640)
+    dpg.set_viewport_height(390)
+    # Set the RemoteRCON window as the primary window
+    dpg.set_primary_window("RemoteRCONWindow", True)
+    dpg.set_global_font_scale(1)
+    
 
-            
-dpg.create_viewport(title='RemoteRCON', width=640, height=390)
-dpg.setup_dearpygui()
-dpg.set_viewport_resizable(False)
-dpg.show_viewport()
-dpg.set_primary_window("RemoteRCON", True)
-dpg.start_dearpygui()
 
-dpg.destroy_context()
+auth()
