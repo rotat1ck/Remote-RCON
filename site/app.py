2fe61b6dbe05d0c63b36db993eb69947837334c7
rotat1ck <rotatick32151@gmail.com>
2025-01-01 07:51:25 +0300

    added comments on main app

--- site/app.py
+++ site/app.py
@@ -1,9 +1,8 @@
-from flask import Flask, render_template, request, jsonify, session, redirect, url_for
-import random, string, requests, json
+from flask import Flask, render_template, request, jsonify, redirect, url_for
+import requests, json
 
 
 app = Flask(__name__)
-
 
 @app.route('/', methods=['GET'])
 def login_page():
@@ -11,8 +10,8 @@ def login_page():
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
+    # получения хэша и имени пользователя
     if request.method == 'POST':
-        # Check if it's a JSON request (from AJAX)
         if request.is_json:
             data = request.json
             print(data)
@@ -20,6 +19,7 @@ def login():
             username = data.get('username')
 
     data = {"hash": hash_value}
+    # отправка в api
     response = requests.post("http://localhost:8000/checkhash", json=data)
 
     if response.status_code == 200:
@@ -28,19 +28,19 @@ def login():
             userid = data.get("id")
             oplvl = data.get("oplvl")
             accesscode = data.get("code")
+            # лог подключения
             print(f"Подключился пользователь: {userid} \nУровень доступа: {oplvl}\nКод: {accesscode}")
             if request.is_json:
                 return jsonify({
                     'access_code': accesscode,
                     'username': username 
                 })
-            
+
             return redirect(url_for('dashboard'))
         except json.JSONDecodeError:
             print("Invalid JSON response:", response.text)
     else:
         print("Error:", response.status_code)
-        # For traditional form submission, you might want to add error handling
         return jsonify({"success": False}), 403
 
             
@@ -51,25 +51,29 @@ def dashboard():
 
 @app.route('/execute', methods=['POST'])
 def execute_command():
+    # получение временного кода и команды от пользователя
     jsondata = request.json
     accesscode = jsondata.get('access_code')
     command = jsondata.get('command', '')
 
     data = {"code": accesscode}
+    # отправка в api
     response = requests.post("http://localhost:8000/main", json=data)
     if response.status_code == 200:
         try:
             data = response.json()
             userid = data.get("id")
             oplvl = data.get("oplvl")
-            code = data.get("accesscode")
             # выполнение rcon команды
             import rconexec
             try:
                 dataToSend = rconexec.check(command, oplvl)
             except TimeoutError:
                 dataToSend = 'Server is down. Use /start'
+            
+            # лог исполняемых команд
             print(f"Пользователь {userid} | Выполнил команду {command}")
+            
             return jsonify({'output': f'{dataToSend}'})
         except json.JSONDecodeError:
             print("Invalid JSON response:", response.text)
@@ -79,13 +83,15 @@ def execute_command():
     
 @app.route('/get_players', methods=['POST'])
 def get_players():
+    # получение временного кода
     accesscode = request.json.get('access_code')
     data = {"code": accesscode}
+    # отправка в api
     response = requests.post("http://localhost:8000/main", json=data)
     
     if response.status_code == 200:
         try:
-            # Simulate getting player list (replace with actual RCON command)
+            # исполнение rcon команды - list
             from rconexec import get_list_of_players
             try:
                 players_response = get_list_of_players()
@@ -93,38 +99,41 @@ def get_players():
                 return jsonify({'output': 'Server is down. Use /start'})
             
             if ':' in players_response:
+                # разделение на префикс (кол-во игроков) и список игроков
                 prefix, player_names = players_response.split(':')
                 player_names = player_names.strip()
                 player_names = player_names.split(', ')
                 
-                # Extract player count
+                # разделение префикса на кол-во игроков и максимальное кол-во игроков
                 words = prefix.split()
                 numbers = [word for word in words if word.isdigit()]
                 
+                # отправка результата
                 return jsonify({
                     'total_players': numbers[0],
                     'max_players': numbers[1],
                     'players': player_names
                 })
             
+            # соединение с сервером не установлено
+            # назначает "0:0" в меню игроков 
             return jsonify({'players': []})
         
         except Exception as e:
             return jsonify({'error': str(e)}), 500
     
-    # Handle non-200 response
     return jsonify({'error': 'Failed to fetch players, status code: {}'.format(response.status_code)}), response.status_code
 
 
 @app.route('/logout', methods=['POST'])
 def logout():
-    # Clear the access code
     
     return jsonify({'success': True})
 
+# запуск приложения
 if __name__ == '__main__':
     app.run(
-        host='0.0.0.0',  # Listen on all available network interfaces
-        port=7777,       # Choose your desired port
-        debug=True       # Set to False in production
+        host='0.0.0.0', # оставьте '0.0.0.0' или выберите нужный вам интерфейс
+        port=7777, # измените на нужный вам порт
+        debug=False
     )
